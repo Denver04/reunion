8ff883f883103d1068dac860bd86efe48465252b
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-02-17 15:14:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <h1>hello</h1> */}
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import "../CSS/navbar.css"
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='navbar-icon'>
+           <HomeIcon className='icon' />
+           <h5>Reunion</h5> 
+        </div>
+        <div className='navbar-tags'>
+            <Button variant="text">Rent</Button>
+            <Button variant="text">Buy</Button>
+            <Button variant="text">Sell</Button>
+            <Button variant="text">Manage property</Button>
+            <Button variant="text">Resources</Button>
+        </div>
+        <div className='navbar-btn'>
+            <Button variant="outlined">Login</Button>
+            <Button variant="contained" style={{backgroundColor:"rgb(124 77 255)" , color:"white"}}>Signup</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

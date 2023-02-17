bee03eff550b23239e1439f3ea8df43962461060
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-02-17 15:26:36 +0530

    input bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Body from './Components/Body';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       {/* <h1>hello</h1> */}
       <Navbar />
+      <Body />
     </div>
   )
 }
---
+++ src/Components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../CSS/body.css";
+
+function Body() {
+  return (
+    <div className='body'>
+        <div className='body1'>
+            <h1>Search properties to rent</h1>
+            <input type="search" placeholder='Search'></input>
+        </div>
+    </div>
+  )
+}
+
+export default Body
